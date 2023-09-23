e3f6fc52a68c694581decee3fa914e942c808a77
Emil <emil.reyier.hedin@gmail.com>
2023-09-23 17:05:00 +0200

    Servorna rör på sig

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,45 @@
 #include "Wire.h"
 #include "Arduino.h"
 #include <Adafruit_ADS1X15.h>
+#include "../.pio/libdeps/esp32s3box/ESP32Servo/src/ESP32Servo.h"
 
 Adafruit_ADS1115 ads;
 TwoWire I2C = TwoWire(0);
+
+Servo multiBottom;
+Servo multiTop;
 
 void setup() {
     I2C.begin(8, 9);
     Serial.begin(115200);
     ads.setGain(GAIN_SIXTEEN);
     ads.begin(0x48, &I2C);
+
+    ESP32PWM::allocateTimer(0);
+    ESP32PWM::allocateTimer(1);
+
+    multiBottom.setPeriodHertz(50);
+    multiBottom.attach(41, 500, 2500);
+    multiTop.attach(42, 500, 2500);
 }
 
 void loop() {
+    for (int pos = 0; pos <= 180; pos += 1) {
+        multiBottom.write(pos);
+        multiTop.write(180 - pos);
+        delay(15);
+    }
+    for (int pos = 180; pos >= 0; pos -= 1) {
+        multiBottom.write(pos);
+        multiTop.write(180 - pos);
+        delay(15);
+    }
+
+    delay(50);
+
+    multiBottom.write(90);
+    multiTop.write(90);
+    delay(1000);
     int16_t adc0;
 
     adc0 = ads.readADC_SingleEnded(0);
