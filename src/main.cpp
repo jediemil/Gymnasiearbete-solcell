848cb4e635b747fd2a059d05b563a0e3f505fcb0
jediemil <emil.reyier.hedin@gmail.com>
2023-10-03 17:31:03 +0200

    Lägg till status LED och alla komponenter på kretskortet.

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,41 @@
-#include "SPI.h"
 #include "Wire.h"
 #include "Arduino.h"
 #include <Adafruit_ADS1X15.h>
 #include "../.pio/libdeps/esp32s3box/ESP32Servo/src/ESP32Servo.h"
+#include "../.pio/libdeps/esp32s3box/NeoPixelBus/src/NeoPixelBus.h"
 
-Adafruit_ADS1115 ads;
+Adafruit_ADS1115 ads1;
+Adafruit_ADS1115 ads2;
 TwoWire I2C = TwoWire(0);
 
 Servo multiBottom;
 Servo multiTop;
 
+Servo singleBottom;
+Servo singleTop;
+
+NeoPixelBus<NeoBgrFeature, NeoEsp32Rmt0800KbpsMethod> statLED(1, 38);
+
 void setup() {
-    I2C.begin(8, 9);
     Serial.begin(115200);
-    ads.setGain(GAIN_SIXTEEN);
-    ads.begin(0x48, &I2C);
+
+    statLED.Begin();
+    statLED.ClearTo(RgbColor(255, 255, 0));
+    statLED.Show();
+
+    I2C.begin(8, 9);
+
+    ads1.setGain(GAIN_SIXTEEN);
+    ads1.begin(0x48, &I2C);
+
+    ads2.setGain(GAIN_SIXTEEN);
+    ads2.begin(0x49, &I2C);
 
     ESP32PWM::allocateTimer(0);
     ESP32PWM::allocateTimer(1);
+    ESP32PWM::allocateTimer(2);
+    ESP32PWM::allocateTimer(3);
 
-    multiBottom.setPeriodHertz(50);
     multiBottom.attach(41, 500, 2500);
     multiTop.attach(42, 500, 2500);
 }
@@ -41,11 +57,11 @@ void loop() {
     multiBottom.write(90);
     multiTop.write(90);
     delay(1000);
-    int16_t adc0;
+    int16_t adc1_0;
 
-    adc0 = ads.readADC_SingleEnded(0);
+    adc1_0 = ads1.readADC_SingleEnded(0);
     //Serial.println(adc0);
-    float volts0 = ads.computeVolts(adc0); //adc0 / (float) 0x8000 * 0.256;
+    float volts0 = ads1.computeVolts(adc1_0); //adc0 / (float) 0x8000 * 0.256;
     /*Serial.print("AIN0: ");
     Serial.println(adc0);
     Serial.print("VOLT0: ");
